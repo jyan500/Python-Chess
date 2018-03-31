f742dc9ac4a603e05a55c41729140451bf2bd3c7
Jansen Yan <yanjl@uci.edu>
2018-03-31 01:07:20 -0700

    added board

--- model.py
+++ model.py
@@ -1,5 +1,28 @@
 ''' chess v3, Jansen Yan '''
-
+class Board:
+    def __init__(self, xDim, yDim):
+        ''' make an 8 x 8 array '''
+        self.xDim = xDim
+        self.yDim = yDim
+        self.board = [ ["*"] * self.xDim] * self.yDim
+        
+    def __str__(self):
+        ''' print(Board object) should print board with algebraic notation'''
+        boardStr = ""
+        alphabet = ["a", "b", "c", "d", "e", "f", "g", "h"]
+        numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+        ''' print numbers on the side of the board '''
+        
+        for i in range(self.xDim):
+            ''' start in reverse to get 8 first, then 7, 6, etc'''
+            boardStr += str(numbers[-i-1]) + " "
+            for j in range(self.yDim):
+                boardStr += self.board[i][j] + " " 
+            boardStr += "\n"
+        boardStr += "  "
+        for i in range(len(alphabet)):
+            boardStr += alphabet[i] + " "
+        return boardStr
 class Piece:
     def __init__(self):
         self.x = 0
@@ -26,7 +49,10 @@ class Pawn(Piece):
         self.hasMoved = False
         
     def checkValidMove(self, x, y):
-        ## if color is white, allow move twice
-        if self.color == "W":
+        ## if pawn hasn't moved, it can move twice
+        pass
+
+b = Board(8,8)
+print(b)
             
-    
+            
